5a9805182087d849b42316f011910c5d8eb1c081
BradleyCooper-IOS <bradley.cooper@traffgrunt.com>
2026-03-30 14:00:38 +0300

    Initial commit

---
+++ TestCodeMagic/TestCodeMagicApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCodeMagicApp.swift
+//  TestCodeMagic
+//
+//  Created by Nick on 30/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct TestCodeMagicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
